e65ffd25a1e11d8f8a78c866e93af5f0c90b05ea
quantoor <ema.bolo94@gmail.com>
2024-03-01 00:53:36 +0200

    Implemented controller

---
+++ src/controller.rs
@@ -0,0 +1,46 @@
+use std::str::FromStr;
+use crate::{compute_gas_fee_eth, RPC_URL_HTTP};
+use actix_web::http::header::ContentType;
+use actix_web::{get, web, HttpResponse, Responder};
+use ethers::middleware::Middleware;
+use ethers::prelude::{Http, Provider, H256};
+
+#[derive(Clone)]
+pub struct Application {
+    pub client: Provider<Http>,
+}
+
+impl Application {
+    pub fn new() -> anyhow::Result<Application> {
+        Ok(Self {
+            client: Provider::<Http>::try_from(RPC_URL_HTTP).unwrap(),
+        })
+    }
+}
+
+#[get("/")]
+async fn home(controller: web::Data<Application>) -> impl Responder {
+    HttpResponse::Ok()
+        .content_type(ContentType::plaintext())
+        .body("noice!")
+}
+
+#[get("/tx_fee")]
+async fn tx_fee(controller: web::Data<Application>) -> impl Responder {
+    let tx_hash =
+        H256::from_str("0x465a5e24ebe4ad90d1a235455f14a12b4aba4b956893d4bf11d0d986ee42c4a7") // fixme
+            .unwrap();
+    let tx = controller
+        .client
+        .get_transaction_receipt(tx_hash)
+        .await
+        .unwrap(); // todo handle this
+    if tx == None {
+        panic!("tx hash not found") // fixme
+    }
+    let tx = tx.unwrap();
+    let fee = compute_gas_fee_eth(&tx).await.unwrap();
+    HttpResponse::Ok()
+        .content_type(ContentType::plaintext())
+        .body(format!("{}", fee))
+}
--- tests/fee.rs
+++ tests/fee.rs
@@ -6,6 +6,20 @@ use std::str::FromStr;
 use uniswap_watcher::{compute_gas_fee_eth, RPC_URL_HTTP};
 
 #[tokio::test]
+async fn get_tx_receipt() {
+    let client = Provider::<Http>::try_from(RPC_URL_HTTP).unwrap();
+    let tx_hash =
+        H256::from_str("0xe55abfa818e6237b794a41a99482ef7108ed7d6c89867ed9b443011c93d2fb77")
+            .unwrap();
+    let receipt = client
+        .get_transaction_receipt(tx_hash)
+        .await
+        .unwrap()
+        .unwrap();
+    println!("{receipt:?}");
+}
+
+#[tokio::test]
 async fn gas_fee_eth() {
     let client = Provider::<Http>::try_from(RPC_URL_HTTP).unwrap();
     let tx_hash =
